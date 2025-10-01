bf34031630a95ed436acbc789545ee2e7717e2c4
syvixor <syvixor@proton.me>
2025-10-01 10:12:53 +0100

    chore: add compatibility date in nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: ["@nuxt/ui", "@vueuse/nuxt"],
+    compatibilityDate: "latest",
     nitro: { compatibilityDate: "latest" },
     devtools: { enabled: true },
     colorMode: { preference: "dark" },
